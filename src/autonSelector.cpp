e64c17b241dcf47f11a5ec9cb9da7038fd890b42
dryder5403 <60204263+dryder5403@users.noreply.github.com>
2021-01-28 15:27:32 -0800

    it should w o r k now

---
+++ src/autonSelector.cpp
@@ -0,0 +1,69 @@
+/*Goal:
+  -Make two buttons (Alliance Buttons)
+  -Position from front wall entry (in)
+  -Skills
+  ----------------------------------------------
+  -Ball placement buttons
+*/
+#include "vex.h"
+#include "robot-config.h"
+
+
+
+
+using namespace vex;
+
+brain Brain;
+brain::lcd Screen;
+// int xpmin1 = 50;
+// int xpmax1 = 150;
+// int ypmin1 = 50;
+// int ypax1 = 150;
+// int xpmin2 = 200;
+// int xpmax2 = 300;
+// int ypmin2 = 50;
+// int ypax2 = 150;
+
+struct rectStruct{
+  int startx;
+  int starty;
+  int lengthx;
+  int lengthy;
+};
+
+rectStruct blueSquare;
+rectStruct redSquare;
+
+
+void drawing(){
+  Brain.Screen.setPenColor(blue);
+  Screen.drawRectangle(blueSquare.startx,blueSquare.starty,blueSquare.lengthx,blueSquare.lengthy,blue);
+  Brain.Screen.setPenColor(red);
+  Screen.drawRectangle(redSquare.startx,redSquare.starty,redSquare.lengthx,redSquare.lengthy,red);
+  //Brain.Screen.setFillColor(blue);
+}
+  void squareParam(){
+    blueSquare.startx = 50;
+    blueSquare.starty = 50;
+    blueSquare.lengthx = 100;
+    blueSquare.lengthy = 100;
+    redSquare.startx = 200;
+    redSquare.starty = 50;
+    redSquare.lengthx = 100;
+    redSquare.lengthy = 100;
+    
+    }
+    
+    bool squareCheck(){
+
+    int xPos = Brain.Screen.xPosition();
+    int yPos = Brain.Screen.xPosition();
+  if(Brain.Screen.pressing() && xPos >= blueSquare.startx && xPos < blueSquare.lengthx && yPos > blueSquare.starty && yPos < redSquare.lengthy){
+    return true;
+  }
+      return false;
+  
+     }
+  
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,27 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
+/*Goal:
+  -Make two buttons (Alliance Buttons)
+  -Position from front wall entry (in)
+  -Skills
+  ----------------------------------------------
+  -Ball placement buttons
+*/
+
+
+
 // ---- START VEXCODE CONFIGURED DEVICES ----
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
+#include "autonSelector.h"
 
 using namespace vex;
 
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
-  
+  drawing();
+  squareBuilder();
 }
